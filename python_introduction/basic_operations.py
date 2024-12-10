79cd1016792cb8b026450ef896983a4b47e8bf8d
gideonagbavor8 <gideonagbavor8@gmail.com>
2024-12-10 13:25:10 +0000

    Introduction to Python: Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Basic Arithmetic Exercise
+number1 = 10
+number2 = 5
+
+sum = number1 + number2 
+
+difference = number1 - number2
+
+product = number1 * number2
+
+print(f"Sum of {number1} and {number2} is {sum}")
+print(f"Difference of {number2} and {number1} is {difference}")
+print(f"Product of {number1} and {number2} is {product}")
+
