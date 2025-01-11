14442fdc0a5ac7d1a311f18b45de898b0e5dd772
gideonagbavor8 <gagbavor@byupathway.edu>
2025-01-11 13:47:54 +0000

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,7 +2,6 @@
 # Created: 12.06.2021
 # Purpose: A simple tool to convert temperature from one unit to another
 
-# temp_conversion_tool.py
 
 # Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
@@ -18,17 +17,17 @@ def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-def main(): 
-
+def main():
     temperature = float(input("Enter the temperature to convert: ")) 
-    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper() 
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
     if unit == 'C': 
-        converted_temp = convert_to_fahrenheit(temperature) 
+        converted_temp = convert_to_fahrenheit(temperature)
         print(f"{temperature}°C is {converted_temp}°F") 
     elif unit == 'F': 
         converted_temp = convert_to_celsius(temperature) 
         print(f"{temperature}°F is {converted_temp}°C") 
     else: 
-        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.") 
+        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
 if __name__ == "__main__":
     main()
